6e35e1b54951c625da4f9192504ab2c6b4c00a44
sanyal10 <sanyaldebajyoti2003@gmail.com>
2024-07-23 15:01:21 +0530

    First commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  createBrowserRouter,
+  RouterProvider,
+  Link
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+   <RouterProvider router={router} />
+  </React.StrictMode>
+);
